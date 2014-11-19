ca908b3f10477eebce195c6d5c1bbcdfd8b1ef9c
kaiix <kvn.hou@gmail.com>
2014-11-20 00:17:50 +0800

    Add custom "File Templates"
    Use ___USERNAME___ instead ___FULLUSERNAME___

---
+++ Templates/File Templates/Kobo/Cocoa Touch Class.xctemplate/UICollectionReusableViewObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,13 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___USERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+@end
---
+++ Templates/File Templates/Kobo/Objective-C File.xctemplate/Empty File/___FILEBASENAME___.m
@@ -0,0 +1,9 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___USERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import <Foundation/Foundation.h>
